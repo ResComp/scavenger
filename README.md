0c6d46ccabb01f5e8f41847292d2a63dbf0a69cd
Nicholas Sun <olassun2@gmail.com>
2016-04-01 10:46:26 -0700

    scavenger: Create the initial framework along with a sample task

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
 * `vagrant up`
 * `vagrant ssh`
 * `$ bash /vagrant/scavenger.sh`
+
+# How to write a task
+
+Create an executable file in `tasks/` that accepts 3 commands:
+`setup`, `run`, `cleanup`.
+
+```
+# For a task "sampleTask"
+
+$ sampleTask setup
+$ sampleTask run 
+$ sampleTask cleanup
+
+# Must all be valid commands
+```
--- scavenger.sh
+++ scavenger.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo ln -sf "/vagrant" "/scavenger"
+source "/scavenger/lib/libbash"
+
 # So what do we want to teach them about?
 # - XXX READING THE LOGS
 # - shell tools
@@ -15,28 +18,84 @@
 # - process that spawns others that print to screen, have them use ps, tr, cut,
 #   and kill
 
-text() {
-    setting="$1" # "0" = bold, "1" = blue, "2" = red
-    message=$2
+newChapter() {
+    title=$1
 
-    normal=$(tput sgr0)
-    different=$(tput bold)
-    if [ "$setting" = "1" ]; then
-        different="${different}$(tput setaf 4)"
-    elif [ "$setting" = "2" ]; then
-        different="${different}$(tput setaf 1)"
-    fi
-
-    echo -e "${different}${message}${normal}"
+    Announce 0 "Chapter $curChapter: $title"
+    curChapter=$((curChapter + 1))
+    while true; do
+        read -p "$(Text 0 "Do you want to skip this chapter? ")" yn
+        case $yn in
+            yes ) return 1;;
+            no ) return 0;;
+            * ) Text 2 "Please answer yes or no.\n";;
+        esac
+    done
 }
 
-announce() {
-    setting="$1" # "0" = bold, "1" = blue, "2" = red
-    message=$2
-    
-    text "$setting" "\n${message}\n"
+runTasks() {
+    tasks=$@
+
+    for task in $tasks; do
+        if ! runSingleTask "$task"; then
+            Announce 2 "ERROR: task $task aborted"
+        fi
+    done
+}
+
+runSingleTask() {
+    taskName=$1
+
+    taskFile="$taskDir/$taskName"
+    if [ ! -f "$taskFile" ]; then
+        Text 2 "$taskFile does not exist or is not a file\n"
+        return 1
+    fi
+    if [ ! -x "$taskFile" ]; then
+        Text 2 "$taskFile is not executable\n"
+        return 1
+    fi
+
+    if ! $taskFile "setup"; then
+        $taskFile "cleanup"
+        return 1
+    fi
+    if ! $taskFile "run"; then
+        $taskFile "cleanup"
+        return 1
+    fi
+    if ! $taskFile "cleanup"; then
+        return 1
+    fi
+    return 0
+}
+
+doSetup() {
+    echo -n "Initializing..."
+
+    sudo ln -sf "$sourceDir/submit" "$binDir/submit"
+    sudo rm -rf "$PipeDir"
+    sudo mkdir -p -m 777 "$PipeDir"
+    sudo mkfifo -m 666 $SubmitPipe $ResultPipe $MessagePipe
+
+    echo "Done!"
 }
 
 ### Begin Main Script ###
 
-announce 0 "Welcome to the ResComp Scavenger Hunt!"
+set -e
+
+curChapter=0
+
+binDir="/usr/local/bin"
+sourceDir="/scavenger"
+taskDir="$sourceDir/tasks"
+
+Announce 0 "Welcome to the ResComp Scavenger Hunt!"
+doSetup
+
+if newChapter "Life in the Shell"; then
+    runTasks "sampleTask"
+fi
+
+Announce 1 "You've completed the ResComp Scavenger Hunt!"
